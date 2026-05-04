183be1335db5125bd480b559ecb76fe1dec2cdb8
pramathapaul <pramathapaul8090@gmail.com>
2026-05-04 12:22:30 +0530

    Add files via upload

---
+++ models/OTP.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose'
+
+const otpSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true
+  },
+  otp: {
+    type: String,
+    required: true
+  },
+  expiresAt: {
+    type: Date,
+    required: true
+  },
+  isUsed: {
+    type: Boolean,
+    default: false
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    index: { expires: '10m' } 
+  }
+})
+
+const OTP = mongoose.model('OTP', otpSchema)
+export default OTP
